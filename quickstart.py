cbc54b487ae06f8d7cb9a5567b8711bc9e9b6426
Caio Antonio <caioantonio@montsaintcapital.com>
2021-03-05 10:10:45 -0300

    insta_bot
    add Cloud VM

--- quickstart.py
+++ quickstart.py
@@ -7,15 +7,15 @@ insta_username = ''
 insta_password = ''
 
 comments = ['Nice shot! @{}',
-        'I love your profile! @{}',
+        'Your profile is grate! @{}',
         'Your feed is an inspiration :thumbsup:',
         'Just incredible :open_mouth:',
         'What camera did you use @{}?',
-        'Love your posts @{}',
+        'Great posts @{}',
         'Looks awesome @{}',
-        'Getting inspired by you @{}',
+        'Amazing Pick @{}',
         ':raised_hands: Yes!',
-        'I can feel your passion @{} :muscle:']
+        'Nice pick @{} :muscle:']
 
 # get an InstaPy session!
 # set headless_browser=True to run InstaPy in the background
@@ -29,9 +29,9 @@ with smart_run(session):
   session.set_dont_include(["friend1", "friend2", "friend3"])		
   
   # activity		
-  session.like_by_tags(["natgeo"], amount=10)
+  session.like_by_tags(["programing", "programmer", "fintech", "blockchain"], amount=10)
 
   # Joining Engagement Pods
   session.set_do_comment(enabled=True, percentage=35)
   session.set_comments(comments)
-  session.join_pods(topic='sports', engagement_mode='no_comments')
+  session.join_pods(topic='technology', engagement_mode='no_comments')
